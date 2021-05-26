5b520ddfb033d1ca0d9a2de7234e5e26b2a8c63e
Damanpreetjagdev <daminidamu081@gmail.com>
2021-05-26 22:50:28 +0100

    week5 exercise 4

--- week5/script1.js
+++ week5/script1.js
@@ -1,6 +1,12 @@
 function myclickEvent (){
    var element = document.getElementById('circle');
-   element.style.opacity = 0.5;
+   element.style.opacity = 1;
 }
 
-document.addEventListener('DOMContentLoaded', myclickEvent);
+function myLoadEvent (){
+    var element = document.getElementById('circle');
+    element.addEventListener('click', myclickEvent);
+    element.style.opacity = 0.5;
+}
+
+document.addEventListener('DOMContentLoaded', myLoadEvent);
