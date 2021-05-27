ef4702cea7ddd935131e6b4fdf8350c94af269db
Damanpreetjagdev <daminidamu081@gmail.com>
2021-05-27 02:52:44 +0100

    week5 exercise 9

--- week5/script1.js
+++ week5/script1.js
@@ -1,18 +1,49 @@
-function myKeyDown (event){
-   var element = document.getElementById('circle');
-   var positionLeft = element.offsetLeft;
-
-   element.style.left = positionLeft - 10 + 'px';
-}
-
-function myLoadEvent (){
+function moveUp() {
+    var element = document.getElementById('circle');
+    var positionTop = element.offsetTop;
+    element.style.top = positionTop - 1 + 'px';
+  }
+  
+  function moveDown() {
+    var element = document.getElementById('circle');
+    var positionTop = element.offsetTop;
+    element.style.top = positionTop + 1 + 'px';
+  }
+  
+  function moveLeft() {
+    var element = document.getElementById('circle');
+    var positionLeft = element.offsetLeft;
+    element.style.left = positionLeft - 1 + 'px';
+  }
+  
+  function moveRight() {
+    var element = document.getElementById('circle');
+    var positionLeft = element.offsetLeft;
+    element.style.left = positionLeft + 1 + 'px';
+  }
+  
+  
+  
+  function myKeyDown(event) {
+    if (event.keyCode == 37) {
+      setInterval(moveLeft, 10);
+    }
+    if (event.keyCode == 38) {
+      setInterval(moveUp, 10);
+    }
+    if (event.keyCode == 40) {
+      setInterval(moveDown, 10);
+    }
+    if (event.keyCode == 39) {
+      setInterval(moveRight, 10);
+    }
+  }
+  
+  function myLoadEvent() {
     document.addEventListener('keydown', myKeyDown);
-}
-
-document.addEventListener('DOMContentLoaded', myLoadEvent);
-
-function myKeyDown(event) {
-    console.log(event.keyCode);
-}
-
-document.addEventListener('keydown', myKeyDown);
+  }
+  
+  
+  
+  document.addEventListener('DOMContentLoaded', myLoadEvent);
+  
