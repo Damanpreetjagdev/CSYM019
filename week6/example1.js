9f23824240e10b592112219a251a5454ee0c1a40
Damanpreetjagdev <daminidamu081@gmail.com>
2021-05-28 13:26:51 +0100

    week 6 ex2 - pie chart

--- week6/example1.js
+++ week6/example1.js
@@ -1,16 +1,32 @@
 var element = document.getElementById('myChart').getContext('2d');
 
-var chartType = 'bar';    //The type of chart we want to create
+var chartType = 'pie';    //The type of chart we want to create
 
 // The data for our dataset
 
    var  chartData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
         datasets: [{
             label: 'My First dataset',
-            backgroundColor:'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            data: [20, 10, 5, 2, 20, 30],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+            
         }]
     };
 
